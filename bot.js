7128711f2cfe613e60ad81044a75cc2914a2bd4f
Alex Block <alex@klaserfilms.com>
2020-03-31 17:43:07 -0500

    Add new version

--- bot.js
+++ bot.js
@@ -181,7 +181,7 @@ client.on('message', message => {
               { name: '!help', value: 'Show this help menu.' },
           )
           .setTimestamp()
-          .setFooter('Created by klaser (0.0.2)');
+          .setFooter('Created by klaser (0.0.3)');
 
         message.channel.send(helpMessage);
     }
